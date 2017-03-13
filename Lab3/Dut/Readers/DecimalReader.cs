57935876f698e23d8b3401262f6581b31daf441d
KBalukh <baluh.kostya@gmail.com>
2017-03-13 21:08:09 +0200

    Lab3 append readers

---
+++ Lab3/Dut/Readers/DecimalReader.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab2.Dut.Readers
+{
+    class DecimalReader : AbstractReader<decimal>
+    {
+        protected override bool Validate(string strValue, out decimal value)
+        {
+            return decimal.TryParse(strValue, out value);
+        }
+
+        protected override bool ValidateRange(string strValue, int min, int max, out decimal value)
+        {
+            if (decimal.TryParse(strValue, out value))
+            {
+                if (value >= min && value <= max)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
