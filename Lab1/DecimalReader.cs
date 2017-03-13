fa6b88d2fb7949d67e0dab3eca0a7752000449b3
KBalukh <baluh.kostya@gmail.com>
2017-03-13 20:06:17 +0200

    DecimalReader

---
+++ Lab1/DecimalReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab1
+{
+    class DecimalReader
+    {
+        public decimal Read(string text = null)
+        {
+            decimal result = 0;
+            string strValue = string.Empty;
+            do
+            {
+                Console.Write(text);
+                strValue = Console.ReadLine();
+            }
+            while (!Decimal.TryParse(strValue, out result));
+            return result;
+        }
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab1
 {
@@ -10,6 +6,33 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            DecimalReader decReader = new DecimalReader();
+            //введення вартостi пiдключення живлення на одну робочу станцiю
+            //Console.Write("Введiть вартiсть пiдключення живлення на одну робочу станцiю: ");
+            //string strCostOfPowerPerWStation = Console.ReadLine();
+            //decimal costOfPowerPerWStation = Convert.ToDecimal(strCostOfPowerPerWStation);
+            decimal costOfPowerPerWStation = decReader.Read("Введiть вартiсть пiдключення живлення на одну робочу станцiю: ");
+
+            //введення вартостi пiдключення iнтернету на одну робочу станцiю
+            //Console.Write("Введiть вартiсть пiдключення iнтернету на одну робочу станцiю: ");
+            //string strCostOfInternetPerWStation = Console.ReadLine();
+            //decimal costOfInternetPerWStation = Convert.ToDecimal(strCostOfInternetPerWStation);
+            decimal costOfInternetPerWStation = decReader.Read("Введiть вартiсть пiдключення iнтернету на одну робочу станцiю: ");
+
+            //введення числа робочих станцiй
+            //Console.Write("Введiть число робочих станцiй: ");
+            //string strNumberOfWStation = Console.ReadLine();
+            //decimal numberOfWStation = Convert.ToDecimal(strNumberOfWStation);
+            decimal numberOfWStation = decReader.Read("Введiть число робочих станцiй: ");
+
+            //калькуляцiя заходу
+            decimal costOfAccess = (costOfInternetPerWStation + costOfPowerPerWStation) * numberOfWStation;
+
+            //виведення результату
+            Console.WriteLine("Загальна вартiсть заходу: {0}", costOfAccess);
+
+            //затримка
+            Console.ReadKey();
         }
     }
 }
