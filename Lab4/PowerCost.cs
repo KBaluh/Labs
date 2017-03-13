28117247b99d9d80c60743f76f8dcd86765317e5
KBalukh <baluh.kostya@gmail.com>
2017-03-13 21:58:47 +0200

    Lab4

---
+++ Lab4/PowerCost.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InternetAccessCalculation
+{
+    public class PowerCost : AbstrInstance
+    {
+        bool onceMore = true;
+
+        decimal maxCost = 100M;
+        decimal minCost = 1M;
+
+        private decimal costOfPowerPerWStation;//поле
+
+        public decimal СostOfPowerPerWStation //властивість
+        {
+            set
+            {
+                if (value < minCost || value > maxCost)
+                    onceMore = true;
+                else
+                {
+                    onceMore = false;
+                    costOfPowerPerWStation = value;
+                }
+            }
+            get
+            {
+                return costOfPowerPerWStation;
+            }
+        }
+        
+        StrCheckDecimal strCheckDecimal;
+
+        protected override void Init()
+        {
+            strCheckDecimal = new StrCheckDecimal();
+        }
+        
+        protected override void Idle()
+        {
+            Console.Write("Введiть вартiсть пiдключення живлення на одну робочу станцiю вiд {0} до {1}: ", minCost, maxCost);
+            string strCostOfPowerPerWStation = Console.ReadLine();
+            strCostOfPowerPerWStation = strCheckDecimal.CheckStrDecimal(strCostOfPowerPerWStation, maxCost);
+            СostOfPowerPerWStation = Convert.ToDecimal(strCostOfPowerPerWStation);
+            if (!onceMore)
+                SetDone();
+        }
+
+        protected override void CleanUp()
+        {
+            Console.WriteLine("Выход");
+        }    
+    }
+}
